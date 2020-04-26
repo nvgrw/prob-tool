d0d1203af770f539f49093089cc9dd3d3f66a913
Niklas Vangerow <n@vangerow.net>
2020-04-26 21:57:02 +0100

    add eigen and debug printing

--- include/Analysis.hpp
+++ include/Analysis.hpp
@@ -3,24 +3,39 @@
 
 #include <memory>
 #include <string>
+#include <map>
 
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+#include <llvm/ADT/SmallVector.h>
+
+namespace llvm {
+  class AllocaInst;
+  class Value;
+} // namespace llvm
 
 class Analysis {
 private:
   llvm::LLVMContext Context;
   std::unique_ptr<llvm::Module> Module;
 
+  llvm::SmallVector<const llvm::AllocaInst *, 10> Variables;
+  std::map<const llvm::Value *, unsigned> Labels;
+  unsigned MaxLabels = 0;
+
 public:
   Analysis(const std::string &Filename);
 
 private:
+  bool hasLabel(const llvm::Value *Inst) const;
+
+private:
   void readAndParse(const std::string &Filename);
-
   void prepareModule();
-
+  void computeLabels();
   void computeVariables();
+
+  void printLabeled();
 };
 
 #endif // PT_ANALYSIS_HPP
--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -1,20 +1,53 @@
 
 #include <iostream>
 #include <memory>
+#include <string>
 
 #include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Value.h>
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Transforms/Scalar.h>
+
+#include <Eigen/Dense>
 
 #include "Analysis.hpp"
 
 using namespace llvm;
+using Eigen::MatrixXd;
+
+namespace {
+bool is_labelable(const Instruction *Inst) {
+  switch (Inst->getOpcode()) {
+  case Instruction::Br:
+  case Instruction::Choose:
+  case Instruction::Switch:
+  case Instruction::Store:
+  case Instruction::Select:
+  case Instruction::Ret:
+    return true;
+  default:
+    return false;
+  }
+}
+} // namespace
 
 Analysis::Analysis(const std::string &Filename) {
   readAndParse(Filename);
   prepareModule();
+  computeLabels();
   computeVariables();
+
+  // TODO(nvgrw): change this to accurate reflect # possible values
+  //  unsigned Dimension = 3 * Variables.size();
+
+  printLabeled();
+}
+
+bool Analysis::hasLabel(const llvm::Value *Inst) const {
+  return Labels.find(Inst) != Labels.end();
 }
 
 void Analysis::readAndParse(const std::string &Filename) {
@@ -49,13 +82,56 @@ void Analysis::prepareModule() {
   }
 }
 
+void Analysis::computeLabels() {
+  for (auto &F : Module->functions()) {
+    if (F.isDeclaration())
+      continue;
+
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (!is_labelable(&I))
+          continue;
+
+        Labels[&I] = MaxLabels++;
+      }
+    }
+  }
+}
+
 void Analysis::computeVariables() {
   for (auto &F : Module->functions()) {
     if (F.isDeclaration())
       continue;
 
     for (auto &BB : F)
-      for (auto &I : BB)
-        I.dump();
+      for (auto &I : BB) {
+        AllocaInst *AI = dyn_cast<AllocaInst>(&I);
+        if (!AI)
+          continue;
+
+        Variables.push_back(AI);
+      }
+  }
+}
+
+void Analysis::printLabeled() {
+  for (auto &F : Module->functions()) {
+    if (F.isDeclaration())
+      continue;
+
+    std::printf("(%s)\n", F.getName().data());
+    for (auto &BB : F) {
+      std::printf("%s:\n", BB.getName().data());
+      for (auto &I : BB) {
+        if (hasLabel(&I)) {
+          std::printf("%02u | ", Labels[&I]);
+        } else {
+          std::printf("   | ");
+        }
+        std::cout.flush();
+        I.print(llvm::outs());
+        printf("\n");
+      }
+    }
   }
 }
